d83c0248ee1abfd3b1f72e156f888089d38c0370
Jonnie Spratley <jonnie.spratley@ge>
2018-04-12 14:02:41 -0700

    added jest testing setup for testing client side react components

---
+++ __tests__/setup.js
@@ -0,0 +1,23 @@
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import chai from "chai";
+import chaiJestSnapshot from "chai-jest-snapshot";
+
+import 'core-js/es6/map';
+import 'core-js/es6/set';
+import 'raf/polyfill';
+/*
+import 'jest-styled-components';
+import { mount } from 'enzyme';
+import initStoryshots from '@storybook/addon-storyshots';
+initStoryshots({
+  renderer: mount
+});
+*/
+configure({ adapter: new Adapter() });
+
+global.requestAnimationFrame = function(callback) {
+  setTimeout(callback, 0);
+};
+
+chai.use(chaiJestSnapshot);
